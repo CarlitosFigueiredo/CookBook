e8f98c737668018f4b52ac0e6753546829f1ab90
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2023-10-31 09:05:19 -0300

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
     <header>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ _('Dashboard') }}
+            {{ __('Dashboard') }}
         </h2>
     </header>
 
